ef3532886980579c6e8dcf9bf600ffc731350f73
MarceloBean13 <82844966+MarceloBean13@users.noreply.github.com>
2021-06-29 17:47:28 +0100

    CompareMargin

---
+++ C#Projects/SmallProjects/CompareMargin/CompareMargin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringToCamelCase
+{
+    class CompareMargin
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int CloseCompare(double a, double b, double margin = 0)
+        {
+
+            if (a < b)
+            {
+                if (margin >= b - a && margin != 0 )
+                    return 0;
+                return -1;
+            }
+            else if (a > b)
+            {
+                if (margin >= a - b && margin != 0)
+                    return 0;
+                return 1;
+            }
+            else
+                return 0;
+        }
+    }
+}
